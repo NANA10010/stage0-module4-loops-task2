349b3edd218f3ca12c88d6a842e385a67048f95b
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-02 12:32:26 +0400

    Update FactorialNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -4,7 +4,7 @@ public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {       
         int counter = 1;
         long num = 1;
-        System.out.println(0);
+        System.out.println(1);
         while (counter<=printToInclusive){
             num *= counter;
             System.out.println(num);
