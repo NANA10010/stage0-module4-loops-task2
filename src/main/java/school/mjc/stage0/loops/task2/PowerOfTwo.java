2237e5594b9f7a4aea39868b654a4224923cf9c0
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-02 13:07:43 +0400

    Update PowerOfTwo.java

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -1,17 +1,17 @@
 package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
-    public void powerOfTwo(int power){
-     if(power<0){ System.out.println("too much power");}
-     else {
-         double counter = 0;
-         double num;
-         while (counter<=power){
-             num = Math.pow(2.0,counter);
-             int ans = (int) num;
-             System.out.println(ans);
-             counter++;
-          }
-       }
+    public void printPower(int power) {
+        if(power<0) System.out.println("too much power");
+        else {
+            double counter = 0;
+            double num;
+            while (counter<=power){
+                num = Math.pow(2.0,counter);
+                System.out.println((int)num);
+                counter++;
+            }
+        }
+
     }
 }
