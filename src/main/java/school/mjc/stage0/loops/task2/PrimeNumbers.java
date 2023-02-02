b4cb402aba59405c026d9198a5cdce178a6e18c8
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-02 12:08:06 +0400

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int caunter = 1;
+        while (caunter<=printToInclusive){
+            int num=0;
+            int x=0;
+            while (num<caunter){
+                num++;
+                if (caunter%num==0) x++;
+            }
+
+            if(x==2) System.out.println(caunter);
+
+            caunter++;
+        }
     }
 }
