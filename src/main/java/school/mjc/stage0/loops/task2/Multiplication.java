c1293bfc75a6e8c3d780b4d3ae5658125c396d0a
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-02 12:49:15 +0400

    Update Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,10 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int caunter = 0;
-        while (caunter<=multiplyByAndToInclusive){
-            System.out.println(caunter*multiplyByAndToInclusive);
-            caunter++;
+        if(multiplyByAndToInclusive<0){
+            int caunter = 0;
+            while (caunter>=multiplyByAndToInclusive){
+                System.out.println(caunter*multiplyByAndToInclusive*-1);
+                caunter--;
+            }
+        } else {
+            int caunter = 1;
+            while (caunter <= multiplyByAndToInclusive) {
+                System.out.println(caunter * multiplyByAndToInclusive);
+                caunter++;
+            }
         }
     }
 }
