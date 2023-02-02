8364e73a4fec8e20770096efb5f8d33267a56577
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-02 11:55:12 +0400

    Update NaturalNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+        int caunter = 0;
+        while (caunter<=lastPrinted){
+            System.out.println(caunter);
+            caunter++;
+        }
     }
 }
