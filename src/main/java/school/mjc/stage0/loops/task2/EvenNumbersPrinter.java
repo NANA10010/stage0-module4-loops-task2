9ede5413fa75693278573e65d81b62eda9dc9459
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-02 11:56:42 +0400

    Update EvenNumbersPrinter.java

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
+         int caunter = 0;
+        while (caunter<=lastPrinted){
+            System.out.println(caunter);
+            caunter+=2;
+        }
     }
 }
